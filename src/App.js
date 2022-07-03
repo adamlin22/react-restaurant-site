7f08c88850485c8a19be03ef262714a55fc46969
adamlin22 <adamlin22123@gmail.com>
2022-07-03 12:59:00 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-export default function App() {
+function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
@@ -9,3 +9,5 @@ export default function App() {
     </div>
   );
 }
+
+export default App;
