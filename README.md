63278bb7b683990bae3dfee7c0f8e2b25eff9660
adamlin22 <adamlin22123@gmail.com>
2022-07-03 12:46:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-restaurant-site
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-uvqyfc)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
