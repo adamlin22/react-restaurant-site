3e4b9c569e3cd38cb4bf67765d84ac3c28dd7108
adamlin22 <adamlin22123@gmail.com>
2022-07-04 12:39:42 +0530

    1 file modified, 3 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello Rohit!</h1>
     </div>
   );
 }
